d5836e99b02406eb4aa70ffbb7f1889a4a71a5ac
Christopher Gandrud <christopher.gandrud@gmail.com>
2012-12-04 17:58:14 +0900

    First completed graphs

--- ContainmentGame1.py
+++ ContainmentGame1.py
@@ -19,45 +19,47 @@ UpmL = []
 Signaler1L = []
 Signaler2L = []
 
+#### Create proportion of recoverable assets to all assets ####
+# True value
+OmegaRange = np.random.uniform(0, 0.85, 1000) 
+
 #### Create Two Signaler's Preferences (assume that they are symmetric)
 # Set at +- half gammaHat 
 # Signaler1 = gammaHat/2
 # Signaler2 = -(gammaHat/2)
 
 # Set Signaller 1 wants lower amount than Signaller 2
-Signaler1 = -0.1
-Signaler2 = 0.1
-
-#### Create proportion of recoverable assets to all assets ####
-# True value
-OmegaRange = np.random.uniform(0, 0.85, 1000) 
+S1Range = [-0.1, -0.2]
+S2Range = [0.1, 0.2]
 
 for omega in OmegaRange:
+	for S1 in S1Range:
+		for S2 in S2Range:
 
-	# Expected value 
-	gammaHat = sp.mean(np.random.uniform(0, 0.85, 10000)) 
+			# Expected value 
+			gammaHat = sp.mean(np.random.uniform(0, 0.85, 10000)) 
 
-	# Find if omega falls within [gammaHat + 2*Signaler1, gammaHat + 2*Signaler1]
-	Lower = gammaHat + (2 * Signaler1)
-	Upper = gammaHat + (2 * Signaler2) 
+			# Find if omega falls within [gammaHat + 2*Signaler1, gammaHat + 2*Signaler1]
+			Lower = gammaHat + (2 * S1)
+			Upper = gammaHat + (2 * S2) 
 
-	if  Lower < omega < Upper:
-		guarantee = gammaHat
-	else:
-		guarantee = omega
+			if  Lower < omega < Upper:
+				guarantee = gammaHat
+			else:
+				guarantee = omega
 
-	# Find PM's utility
+			# Find PM's utility
 
-	Xreal = guarantee - omega
+			Xreal = guarantee - omega
 
-	Upm = -(math.pow((0 - Xreal), 2))
+			Upm = -(math.pow((0 - Xreal), 2))
 
-	# Append to lists
+			# Append to lists
 
-	OmegaL.append(omega)
-	UpmL.append(Upm)
-	Signaler1L.append(Signaler1)
-	Signaler2L.append(Signaler2)
+			OmegaL.append(omega)
+			UpmL.append(Upm)
+			Signaler1L.append(S1)
+			Signaler2L.append(S2)
 
 d = {'Omega': OmegaL,
 	'Upm' : UpmL,
--- PlotSimulations.R
+++ PlotSimulations.R
@@ -5,6 +5,30 @@
 # Depends on Python 2.7.2 
 ###############
 
+# Change working directory
+setwd("/git_repositories/ContainmentGame/")
 
+# Load Packages
+library(ggplot2)
 
+# Run game and simulate data
 system('python /git_repositories/ContainmentGame/ContainmentGame1.py')
+
+# Import simulated data
+Sims <- read.csv("SimulatedData/Test.csv")
+
+# Create signaller combinations
+Sims$Signalers[Sims$Signaler1 == -0.1 & Sims$Signaler2 == 0.1] <- "-0.1, 0.1"
+Sims$Signalers[Sims$Signaler1 == -0.1 & Sims$Signaler2 == 0.2] <- "-0.1, 0.2"
+Sims$Signalers[Sims$Signaler1 == -0.2 & Sims$Signaler2 == 0.1] <- "-0.2, 0.1"
+Sims$Signalers[Sims$Signaler1 == -0.2 & Sims$Signaler2 == 0.2] <- "-0.2, 0.2"
+
+
+ggplot(Sims, aes(x = Omega, y = Upm)) +
+        facet_grid(~Signalers) +
+        geom_point() +
+        scale_x_continuous(breaks = c(0.0, 0.4, 0.8), labels = c(0, 0.4, 0.8)) +
+        scale_y_continuous(breaks = c(0.0, -0.05, -0.1, -0.15), labels = c(0, -0.05, -0.1, -0.15)) +
+        xlab("\n Omega") + ylab("Prime Minister's Utility \n") +
+        ggtitle("Prime Minister's Utility \n at various levels of Omega and Signaler Preferences (2 Signalers)")
+        theme_bw(base_size = 12)
